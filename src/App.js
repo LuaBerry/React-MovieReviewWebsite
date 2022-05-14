9e4fca470ac065f67aa49966f1e3708e862b825d
나란히 <lazpberry1012@gmail.com>
2022-05-14 20:20:58 +0900

    Movie app basic function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,55 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: [],
   };
+
+  getMovies = async () => {
+    const{
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+    this.setState({movies, isLoading: false});
+  }
   componentDidMount()
   {
     //Load Movie data.
+    this.getMovies();
   }
-  render() {
-    const {isLoading} = this.state;
+  render()
+  {
+    const {isLoading, movies} = this.state;
     return (
-    <>
-    {isLoading ? 'Loading...' : 'Load complete.'}
-    </>
+    <section class="container">
+      {isLoading ? 
+        (
+          <div  class = "loader">
+            'loading...'
+          </div>
+        )
+        : 
+        (
+          <div class = "movies">
+          {
+            movies.map(
+              (movie) =>
+            {
+              return (
+              <Movie key={movie.id} poster = {movie.medium_cover_image} {...movie}/>
+              )
+            }
+            )
+          }
+          </div>
+        )
+      }
+    </section>
     )
   }
 }
